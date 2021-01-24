07a228c2bd61554f6afbd09462c62e7687dbf719
jjenkins120 <jjenkins120@gmail.com>
2021-01-23 20:09:37 -0600

    added error message to signup login

--- src/signup-screens/InterestedInSignupScreen.js
+++ src/signup-screens/InterestedInSignupScreen.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import { StyleSheet } from 'react-native'
+import { Text } from 'react-native-elements'
 import Form from '../components/Form'
 import FormView from '../components/FormView'
 import useSignupLogin from '../hooks/useSignupLogin'
+import { Context as EntryContext } from '../context/EntryContext'
 
 const InterestedInSignUpScreen = () => {
+    const [loginErrorMessage, setLoginErrorMessage] = useState('')
     const [signupLogin] = useSignupLogin()
+    const { state: { email, password } } = useContext(EntryContext)
 
     return (
         <FormView>
+            <Text>{loginErrorMessage}</Text>
             <Form 
                 placeholder='Interested In?'
                 title='Continue'
-                onPress={signupLogin}    
+                onPress={ email && password ? signupLogin : () => setLoginErrorMessage('Incomplete Profile. Please Re-enter information')}    
             />
         </FormView>
     )
