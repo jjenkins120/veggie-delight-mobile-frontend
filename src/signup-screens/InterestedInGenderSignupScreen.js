6c023d95ebaa2b32fb1ab8a3f1a085cc2d94f490
jjenkins120 <jjenkins120@gmail.com>
2021-01-26 17:42:41 -0600

    added range to interestedage, added checkboxes to interestedveggie and interestedgender

--- src/signup-screens/InterestedInGenderSignupScreen.js
+++ src/signup-screens/InterestedInGenderSignupScreen.js
@@ -1,14 +1,46 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import { StyleSheet } from 'react-native'
 import Form from '../components/Form'
 import FormView from '../components/FormView'
+import { CheckBox } from 'react-native-elements';
 import { Context as EntryContext} from '../context/EntryContext'
 
 const InterestedInGenderSignupScreen = () => {
+    const [checked, setChecked] = useState(false)
     const { addInterestedInGender } = useContext(EntryContext)
+
+    //need to define logic that will added with checked state equally true to compile their titles to an array and add to state
 
     return (
         <FormView>
+            <CheckBox
+                title='Male'
+                checkedIcon='dot-circle-o'
+                uncheckedIcon='circle-o'
+                checked={checked}
+                onPress={()=> setChecked(!checked)}
+            />
+            <CheckBox
+                title='Female'
+                checkedIcon='dot-circle-o'
+                uncheckedIcon='circle-o'
+                checked={checked}
+                onPress={()=> setChecked(!checked)}
+            />
+            <CheckBox
+                title='Trans Male'
+                checkedIcon='dot-circle-o'
+                uncheckedIcon='circle-o'
+                checked={checked}
+                onPress={()=> setChecked(!checked)}
+            />
+            <CheckBox
+                title='Non-Binary'
+                checkedIcon='dot-circle-o'
+                uncheckedIcon='circle-o'
+                checked={checked}
+                onPress={()=> setChecked(!checked)}
+            />
             <Form 
                 placeholder='Interested in Gender' 
                 title='Continue' 
--- src/signup-screens/InterestedInVeggieSignupScreen.js
+++ src/signup-screens/InterestedInVeggieSignupScreen.js
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet } from 'react-native'
 import Form from '../components/Form'
 import FormView from '../components/FormView'
+import { CheckBox } from 'react-native-elements';
 import useSignupLogin from '../hooks/useSignupLogin'
 
 const InterestedInVeggieSignupScreen = () => {
+    const [checked, setChecked] = useState(false)
     const [signupLogin] = useSignupLogin()
+
+    //need to define logic that will added with checked state equally true to compile their titles to an array and add to state
 
     return (
         <FormView>
+            <CheckBox
+                title='Lacto-ovo'
+                checkedIcon='dot-circle-o'
+                uncheckedIcon='circle-o'
+                checked={checked}
+                onPress={()=> setChecked(!checked)}
+            />
+            <CheckBox
+                title='Vegan'
+                checkedIcon='dot-circle-o'
+                uncheckedIcon='circle-o'
+                checked={checked}
+                onPress={()=> setChecked(!checked)}
+            />
+            <CheckBox
+                title='Pescatarian'
+                checkedIcon='dot-circle-o'
+                uncheckedIcon='circle-o'
+                checked={checked}
+                onPress={()=> setChecked(!checked)}
+            />
             <Form 
                 placeholder='Interested In Veggie?'
                 title='Continue'
