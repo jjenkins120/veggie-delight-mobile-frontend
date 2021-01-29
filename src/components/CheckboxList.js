3171fbc2f76321498fcf4b77d3e2cdf69167b929
jjenkins120 <jjenkins120@gmail.com>
2021-01-29 13:04:52 -0600

    added checkboxlist

--- src/components/CheckboxList.js
+++ src/components/CheckboxList.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, View, Text } from 'react-native'
+import { Checkbox } from 'react-native-elements'
 import FormView from './FormView'
 import AppButton from './AppButton'
 
-const CheckboxList = () => {
+const CheckboxList = ({ title, onPress, inputArray }) => {
+    const [state, setState] = useState({})
+
+    useEffect(()=> {
+        inputArray.forEach(input => 
+            setState(state[input] = false)
+        )
+    }, [])
+
+
+    const renderCheckbox = inputArray => {
+        return inputArray.map(input => {
+            return <Checkbox 
+                title={input}
+                checkedIcon='dot-circle-o'
+                uncheckedIcon='circle-o'
+                checked={state.input}
+                onPress={() => setState({...state, input: !state.input})}
+            />
+        })
+    }
+    
+    // const selectionArray = state.filter(input => input[true])
+    
     return (
-        <View></View> 
+        <FormView>
+            {renderCheckbox(inputArray)}
+            <AppButton
+                title={title}
+                onPress={() => onPress(selectionArray)}
+            />
+        </FormView> 
     )
 }
 
--- src/signup-screens/InterestedInGenderSignupScreen.js
+++ src/signup-screens/InterestedInGenderSignupScreen.js
@@ -3,52 +3,69 @@ import { StyleSheet } from 'react-native'
 import Form from '../components/Form'
 import FormView from '../components/FormView'
 import { CheckBox } from 'react-native-elements';
+import CheckboxList from '../components/CheckboxList'
 import { Context as EntryContext} from '../context/EntryContext'
 
 const InterestedInGenderSignupScreen = () => {
-    const [checked, setChecked] = useState(false)
+    // const [checked, setChecked] = useState(false)
     const { addInterestedInGender } = useContext(EntryContext)
 
+    const genderArray = ['Men', 'Women', 'Trans', 'Non-Binary']
     //need to define logic that will added with checked state equally true to compile their titles to an array and sent to the context to be stored in state
 
     return (
-        <FormView>
-            <CheckBox
-                title='Male'
-                checkedIcon='dot-circle-o'
-                uncheckedIcon='circle-o'
-                checked={checked}
-                onPress={()=> setChecked(!checked)}
-            />
-            <CheckBox
-                title='Female'
-                checkedIcon='dot-circle-o'
-                uncheckedIcon='circle-o'
-                checked={checked}
-                onPress={()=> setChecked(!checked)}
-            />
-            <CheckBox
-                title='Trans Male'
-                checkedIcon='dot-circle-o'
-                uncheckedIcon='circle-o'
-                checked={checked}
-                onPress={()=> setChecked(!checked)}
-            />
-            <CheckBox
-                title='Non-Binary'
-                checkedIcon='dot-circle-o'
-                uncheckedIcon='circle-o'
-                checked={checked}
-                onPress={()=> setChecked(!checked)}
-            />
-            <Form 
-                placeholder='Interested in Gender' 
-                title='Continue' 
-                onPress={addInterestedInGender}
-            />
-        </FormView>
+        <CheckboxList
+            title='continue'
+            inputArray={genderArray}
+            onPress={addInterestedInGender}
+        />
     )
 }
+
+// const InterestedInGenderSignupScreen = () => {
+//     const [checked, setChecked] = useState(false)
+//     const { addInterestedInGender } = useContext(EntryContext)
+
+//     //need to define logic that will added with checked state equally true to compile their titles to an array and sent to the context to be stored in state
+
+//     return (
+//         <FormView>
+//             <CheckBox
+//                 title='Male'
+//                 checkedIcon='dot-circle-o'
+//                 uncheckedIcon='circle-o'
+//                 checked={checked}
+//                 onPress={()=> setChecked(!checked)}
+//             />
+//             <CheckBox
+//                 title='Female'
+//                 checkedIcon='dot-circle-o'
+//                 uncheckedIcon='circle-o'
+//                 checked={checked}
+//                 onPress={()=> setChecked(!checked)}
+//             />
+//             <CheckBox
+//                 title='Trans Male'
+//                 checkedIcon='dot-circle-o'
+//                 uncheckedIcon='circle-o'
+//                 checked={checked}
+//                 onPress={()=> setChecked(!checked)}
+//             />
+//             <CheckBox
+//                 title='Non-Binary'
+//                 checkedIcon='dot-circle-o'
+//                 uncheckedIcon='circle-o'
+//                 checked={checked}
+//                 onPress={()=> setChecked(!checked)}
+//             />
+//             <Form 
+//                 placeholder='Interested in Gender' 
+//                 title='Continue' 
+//                 onPress={addInterestedInGender}
+//             />
+//         </FormView>
+//     )
+// }
 
 const styles = StyleSheet.create({
 
