6968e275efc91d418a3663cc9bcc3029c5854646
jjenkins120 <jjenkins120@gmail.com>
2021-01-30 14:42:38 -0600

    latest update

--- src/signup-screens/InterestedInLocationSignupScreen.js
+++ src/signup-screens/InterestedInLocationSignupScreen.js
@@ -1,18 +1,43 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import { StyleSheet } from 'react-native'
 import Form from '../components/Form'
+import { Input, Text } from 'react-native-elements'
 import FormView from '../components/FormView'
+import AppButton from '../components/AppButton'
 import { Context as EntryContext} from '../context/EntryContext'
 
 const InterestedInLocationSignupScreen = () => {
+    const [latitude, setLatitude] = useState('')
+    const [longitude, setLongitude] = useState('')
+    const [radius, setRadius] = useState('')
     const { addInterestedInLocation } = useContext(EntryContext)
 
     return (
         <FormView>
-            <Form 
-                placeholder='Interested in Location' 
-                title='Continue' 
-                onPress={addInterestedInLocation}
+            <Input
+                placeholder='Latitude'
+                value={latitude}
+                autoCapitalize='none'
+                autoCorrect={false}
+                onChangeText={setLatitude}
+            />
+            <Input
+                placeholder='Longitude'
+                value={longitude}
+                autoCapitalize='none'
+                autoCorrect={false}
+                onChangeText={setLongitude}
+            />
+            <Input
+                placeholder='Radius'
+                value={radius}
+                autoCapitalize='none'
+                autoCorrect={false}
+                onChangeText={setRadius}
+            />
+            <AppButton
+                title='Continue'
+                onPress={() => addInterestedInLocation({latitude: parseInt(latitude), longitude: parseInt(longitude), radius: parseInt(radius)})}
             />
         </FormView>
     )
